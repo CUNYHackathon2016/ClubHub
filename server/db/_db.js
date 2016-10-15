40a669b783b528932bdaaea7bf27bd3c657d7c7b
LucasRizzotto <contact@lucasrizzotto.com>
2016-10-15 16:47:55 -0400

    Shittonofchanges
    Don't know what all these are, tbh

--- server/db/_db.js
+++ server/db/_db.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
-var dbURL = process.env.DATABASE_URL || 'postgres://localhost:5432/hackcuny'
+var dbURL = process.env.DATABASE_URL || 'postgres://qlxpxwvn:Unl_RKnPejWVy0PM3eXRJD5aNjeDUsNz@jumbo.db.elephantsql.com:5432/qlxpxwvn'
 
 var db = new Sequelize(dbURL, {
   logging: false
